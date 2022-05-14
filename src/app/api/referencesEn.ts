a1efcfc6e3537ab1189b940e9d029cca9f75720d
Moisés Estevão <moisesestevao2004@hotmai.com>
2022-05-13 23:51:42 -0300

    references update

--- src/app/api/referencesEn.ts
+++ src/app/api/referencesEn.ts
@@ -7,18 +7,11 @@ export const referencesEn = [
       message: "Moisés demonstrated important skills in the area of development. He has been able to adapt to new technologies in a short period of time, which demonstrates that he has a good foundation and mastery of fundamental knowledge that the development area demands. Moisés is a developer with a lot of potential, proactive, dedicated to what he does and who always tries to bring positive results to the team.",
   },
   {
-      personImg: "assets/images/unknown.jpg",
-      personFullname: 'Urias Luís',
-      personPosition: "Development Intern",
-      personCompany: 'Soluarq',
-      message: "Message ....",
-  },
-  {
-    personImg: "assets/images/unknown.jpg",
+    personImg: "assets/images/vanilson.jpg",
     personFullname: 'Vanilson Medeiros',
     personPosition: "Technic Instructor",
     personCompany: 'SENAI',
-    message: "Message ....",
+    message: "Moisés is a student with a professional spirit, dedicated and determined, always willing to face new challenges, always demonstrating proactivity and application. It easily absorbs content from its inspiring grid, I see in it a lot of potential being developed in the correlative areas of programming.",
 }
 ];
 
--- src/app/api/referencesPt.ts
+++ src/app/api/referencesPt.ts
@@ -7,18 +7,11 @@ export const referencesPt = [
         message: "Moisés demonstrou habilidades importantes na área de desenvolvimento. Conseguiu se adaptar a novas tecnologias  num curto período de tempo, o que demonstra possuir uma boa base e domínio de conhecimentos fundamentais que a área de desenvolvimento demanda. Moisés é um desenvolvedor com muito potencial, proativo, dedicado ao que faz e que sempre tenta trazer resultados positivos para a equipe.",
     },
     {
-        personImg: "assets/images/unknown.jpg",
-        personFullname: 'Urias Luís',
-        personPosition: "Estagiário de desenvolvimento",
-        personCompany: 'Soluarq',
-        message: "Mensagem ....",
-    },
-    {
-      personImg: "assets/images/unknown.jpg",
+      personImg: "assets/images/vanilson.jpg",
       personFullname: 'Vanilson Medeiros',
       personPosition: "Instrutor Técnico",
       personCompany: 'SENAI',
-      message: "Mensagem ....",
+      message: "Moisés é um aluno com espirito profissional, dedicado e determinado, sempre disposto a encarar novos desafios, sempre demonstrando proatividade e aplicação. Absorve com facilidade conteúdos de sua grade inspiradora, vejo nele muito potencial sendo desenvolvido nas áreas correlativas de programação.",
   }
 ];
 
